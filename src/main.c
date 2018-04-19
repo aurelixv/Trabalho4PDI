85e73bc7b52058ddf29900a0249e876eb14c66e2
Aurélio Vinícius <aurelio@alunos.utfpr.edu.br>
2018-04-19 02:07:30 -0300

    nova tentativa

--- src/main.c
+++ src/main.c
@@ -5,6 +5,9 @@
 
 #define THRESHOLD 0.6f
 
+//Novas funções
+void mascara(Imagem *original, Imagem *mascara, Imagem *saida);
+
 int main() {
 
     //Localização das imagens a serem segmentadas.
@@ -16,31 +19,91 @@ int main() {
                         "../imagens/205.bmp" 
                        };
     char name[40] = "";
-    Imagem *img, *buffer;
+    Imagem *original, *entrada, *saida, *buffer;
+    Imagem *kernel = criaKernelCircular(7);
+    ComponenteConexo *componente;
 
     //Procedimento para cada imagens.
     for(int i = 0; i < 5; i += 1) {
 
         //Carregando imagem em escala de cinza.
-        img = abreImagem(imagens[i], 1);
+        original = abreImagem(imagens[i], 1);
         sprintf(name, "../resultados/%d1 - Cinza.bmp", i);
-        salvaImagem(img, name); 
+        salvaImagem(original, name); 
 
         //Criando imagens auxiliares, com o mesmo tamanho da imagem carregada.
-        buffer = criaImagem(img->largura, img->altura, img->n_canais);
+        entrada = criaImagem(original->largura, original->altura, original->n_canais);
+        copiaConteudo(original, entrada);
+        saida = criaImagem(original->largura, original->altura, original->n_canais);
+        buffer = criaImagem(original->largura, original->altura, original->n_canais);
 
-        filtroGaussiano(img, img, 5, 5, NULL);
+        filtroGaussiano(entrada, saida, 5, 5, buffer);
+        sprintf(name, "../resultados/%d2 - borrada.bmp", i);
+        salvaImagem(saida, name);
+        copiaConteudo(saida, entrada);
+
+        normalizaSemExtremos8bpp(entrada, saida, 0, 1, 0.01f);
+        sprintf(name, "../resultados/%d3 - normalizada.bmp", i);
+        salvaImagem(saida, name);
+        copiaConteudo(saida, entrada);
+
+        binarizaAdapt(entrada, saida, 101, 0.2, buffer);
+        sprintf(name, "../resultados/%d4 - binAdapt.bmp", i);
+        salvaImagem(saida, name);
+        copiaConteudo(saida, entrada);
+
+        mascara(original, entrada, saida);
+        sprintf(name, "../resultados/%d5 - mascara.bmp", i);
+        salvaImagem(saida, name);
+        copiaConteudo(saida, entrada);
+
+        binariza(entrada, saida, 0.7f);
+        sprintf(name, "../resultados/%d6 - binarizada.bmp", i);
+        salvaImagem(saida, name);
+        copiaConteudo(saida, entrada);
+        
+        erode(entrada, kernel, criaCoordenada(4, 4), saida);
+        sprintf(name, "../resultados/%d7 - Erode.bmp", i);
+        salvaImagem(saida, name);
+        copiaConteudo(saida, entrada);
+
+        printf("Numero de graos: %d\n", rotulaFloodFill(entrada, &componente, 0, 0, 0));
+        
+
+        /* filtroGaussiano(original, saida, 5, 5, NULL);
         sprintf(name, "../resultados/%d2 - Borrada.bmp", i);
-        salvaImagem(img, name);
+        salvaImagem(saida, name);
 
-        binariza(img, img, THRESHOLD);
-        sprintf(name, "../resultados/%d3 - Binarizada.bmp", i);
-        salvaImagem(img, name);
+        
+        Imagem *kernel = criaKernelCircular(11);
+        salvaImagem(kernel, "kernel.bmp");
+        erode(img, kernel, criaCoordenada(6, 6), buffer);
+        sprintf(name, "../resultados/%d3 - Erode.bmp", i);
+        salvaImagem(buffer, name);
+
+        binariza(buffer, buffer, THRESHOLD);
+        sprintf(name, "../resultados/%d4 - Binarizada.bmp", i);
+        salvaImagem(buffer, name); */
 
         //Desalocando memória previamente alocada.
-        destroiImagem(img);
+        destroiImagem(original);
+        destroiImagem(saida);
         destroiImagem(buffer);
     }
 
     return 0;
+}
+
+//Função para subtrair a imagem original da mascara, colocando o resultado na saida.
+void mascara(Imagem *original, Imagem *mascara, Imagem *saida) {
+
+    for(int y = 0; y < original->altura; y += 1) {
+        for(int x = 0; x < original->largura; x += 1) {
+            if(mascara->dados[0][y][x] == 1.0f)
+                saida->dados[0][y][x] = original->dados[0][y][x];
+            else
+                saida->dados[0][y][x] = 0.0f;
+        }
+    }
+
 }
